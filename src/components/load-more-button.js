095a281b1bd08fa9c578da6740fff5987ad2a043
Kemran Rustamov <rustamovkm@gmail.com>
2022-12-27 12:41:36 +0300

    step 2

---
+++ src/components/load-more-button.js
@@ -0,0 +1,5 @@
+export const createLoadMOreBtnTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`, //Режим сборки
+  entry: `./src/main.js`, // Точка входя для приложения
+  output: { // Настрока для выходного файла
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`, // Подключаем source-map
+  devServer: {
+    static: path.join(__dirname, `public`), // Где искать сборку
+    // Автоматическая перезагрузка страницы
+    // По умолчанию приложение будет доступно по адресу http://localhost:8080
+    // Лучше открывать в режиме инкогнито, чтобы брузер не кэшировал файл сборки
+    // watchContentBase: true
+  }
+}
